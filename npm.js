046223ed02849e8fccf82b302be73266be143653
jesusgarcia149 <jesus.g.jg7@gmail.com>
2023-08-07 17:54:04 -0400

    .

--- npm.js
+++
@@ -1,6 +0,0 @@
-const colors = require('colors');
-
-console.log("hello world".bgGreen);
-console.log("hello world".bgBlue);
-console.log("hello world".bgRed);
-console.log("hello world".bgMagenta);
