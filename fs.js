a1088968e3d87596c1ffe5eb6ddd93fde1bbc87c
jesusgarcia149 <jesus.g.jg7@gmail.com>
2023-08-07 12:40:19 -0400

    .

--- fs.js
+++ fs.js
@@ -7,6 +7,12 @@ const third = fs.readFileSync('./data/third.txt', 'utf-8');
 console.log(first);
 console.log(second);
 
-fs.writeFileSync('./data/third.txt', 'este es un tercer archivo');
+// fs.writeFileSync('./data/third.txt', 'este es un tercer archivo');
+
+const title = 'este contenido se ha añadido';
+
+fs.writeFileSync('./data/fourth.txt', title, {
+    flag: 'a'
+})
 
 console.log(third);
