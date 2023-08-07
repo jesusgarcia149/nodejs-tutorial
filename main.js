529b791270d1e95d6dff1781df3821b98ac1a39c
jesusgarcia149 <jesus.g.jg7@gmail.com>
2023-08-07 11:53:08 -0400

    .

---
+++ main.js
@@ -0,0 +1,8 @@
+const math = require("./math/index");
+
+console.log(math);
+
+console.log(math.add(10, 200));
+console.log(math.subtract(10, 200));
+console.log(math.multiply(10, 200));
+console.log(math.divide(10, 200));
