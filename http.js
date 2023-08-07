da282fcd067211ad2dad8dc68c9481f97236c92e
jesusgarcia149 <jesus.g.jg7@gmail.com>
2023-08-07 13:04:26 -0400

    -

--- http.js
+++ http.js
@@ -1,8 +1,24 @@
 const http = require('http');
 
 http.createServer(function (request, response) {
-    response.write('Hello World');
+   
+    console.log(request.url);
+
+    if (request.url === '/'){
+        response.write('Welcome to the Server');
+        return response.end();
+    }
+
+    if (request.url === '/about'){
+        response.write('Welcome to About');
+        return response.end();
+    }
+
+    response.write(`
+        <h1>Not Found</h1>
+    `)
     response.end();
+
 }).listen(3000);
 
 console.log('Server listen in port 3000');
