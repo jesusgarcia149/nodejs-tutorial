648c729030b7547942d1d30b1f06f152b84ed0c5
jesusgarcia149 <jesus.g.jg7@gmail.com>
2023-08-07 23:07:15 -0400

    .

---
+++ esmodule.js
@@ -0,0 +1,11 @@
+import math, {add,divide,multiply,subtract} from "./math.js";
+
+console.log(math2.add(10,20));
+console.log(math2.subtract(10,20));
+console.log(math2.multiply(10,20));
+console.log(math2.divide(10,20));
+
+console.log(add(10,20));
+console.log(subtract(10,20));
+console.log(multiply(10,20));
+console.log(divide(10,20));
---
+++ math.js
@@ -0,0 +1,22 @@
+export function add (x, y){
+    return x + y
+}
+
+export function subtract (x, y){
+    return x - y
+}
+
+export function multiply (x, y){
+    return x * y
+}
+
+export function divide (x, y){
+    return x / y
+}
+
+export default {
+    add,
+    subtract,
+    multiply,
+    divide
+}
